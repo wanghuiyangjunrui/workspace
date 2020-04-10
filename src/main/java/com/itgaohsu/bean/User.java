aa9aa951a946ac1ca59577df8b41b035ac5fdb0a
Administrator <caochuanxia@163.com>
2020-04-10 14:07:52 +0800

    新增用户类

---
+++ src/main/java/com/itgaohsu/bean/User.java
@@ -0,0 +1,39 @@
+package com.itgaohsu.bean;
+
+/**
+ * @author 曹小花
+ * @Title: User
+ * @Description: SpringCloudAlibaba课程
+ * @date 2020/4/10 0010 14:06
+ */
+public class User {
+    private int id;
+
+    private String loginname;
+
+    private String  password;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getLoginname() {
+        return loginname;
+    }
+
+    public void setLoginname(String loginname) {
+        this.loginname = loginname;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
